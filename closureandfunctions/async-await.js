5c98d07b74b7a8e6db9b23caf4cabc69f6f3356d
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-08-14 00:33:57 -0300

    Create async-await.js

---
+++ closureandfunctions/async-await.js
@@ -0,0 +1,39 @@
+function time(ms){
+    return  new Promise((resolve, reject)=> {
+        setTimeout(()=> resolve(ms), ms)
+    })}
+
+
+
+
+// o async transforma a função em uma promise
+async function ola (){
+// estanciei a função time de forma asyncrona 
+    const ms = await time(2000)
+    console.log('terminou',ms)
+
+}
+console.log(ola())
+
+const fs = require('fs')
+
+//função com uma promessa path se transformará em lisa na proxima função
+function readdir(path) {
+    //retorna uma funçao promise onde os parâmetro resolve e reject servirão  pra gerar uma instrução a ser  manifestada de acordo com a chamada da proxima função hey()
+    return new Promise((resolve, reject)=>{
+        fs.readdir(path, function(err,lista){
+            if(err){
+                reject(err)
+            }else{
+                resolve(lista)
+            }
+        })
+    })
+}
+async function hey (){
+    //a função readdir pra ler o diretorio será estanciada na const lista 
+const lista = await readdir('./')
+// o retorn da função hey() é esse console log  caso der errado caira no reject e mostrará o error caso passe mostrará a lista
+console.log(lista)
+}
+console.log(hey())
